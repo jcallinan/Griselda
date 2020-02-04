ed44882de96779ae69daab6c104102392c5bf877
jcallinan <jeremy.callinan@gmail.com>
2020-02-04 18:58:26 -0500

    First checkin
    Griselda, real rap music. http://www.griseldaxfr.com/

---
+++ GriseldaExample/GriseldaExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GriseldaExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GriseldaMember BennyTheButcher = new GriseldaMember("Benny The Butcher");
+            GriseldaMember WestSideGunn = new GriseldaMember("Westside Gunn");
+            GriseldaMember ConwayTheMachine = new GriseldaMember("Conway The Machine");
+        }
+    }
+}
